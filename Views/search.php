0416a7d36a7d4858b272294090e6ec519edccff0
SONOE385 <kotarou385@gmail.com>
2022-02-12 19:07:25 +0900

    検索機能

--- Views/search.php
+++ Views/search.php
@@ -1,35 +1,3 @@
-<?php
-//設定関連を読み込む
-include_once('../config.php');
-//便利な関数
-include_once('../util.php');
-
-////////ツイート一覧エリアを動的にする/////////
-$view_tweets = [
-    ['user_id'=>1,
-     'user_name'=>'taro',
-     'user_nickname'=>'太郎',
-     'user_image_name'=>'sample-person.jpg',//アイコン画像
-     'tweet_body'=>'今プログラミングをしています。',//つぶやき本文
-     'tweet_image_name'=>null,//投稿画像。1件目は投稿がないのでnull
-     'tweet_created_at'=>'2022-01-22 14:00:00',//投稿日時
-     'like_id'=>null,//自分がいいねしていたら入ってくるid
-     'like_count'=>0,//いいね数
-    ],
-      ['user_id'=>2,
-      'user_name'=>'jiro',
-      'user_nickname'=>'次郎',
-      'user_image_name'=>null,//アイコン画像なし
-      'tweet_body'=>'コワーキングスペースをオープンしました!',//つぶやき本文
-      'tweet_image_name'=>'sample-post.jpg',
-      'tweet_created_at'=>'2021-03-14 14:00:00',//投稿日時
-      'like_id'=>1,//自分がいいねしていたら入ってくるid
-      'like_count'=>1,//いいね数
-      ]
-];
-
-?>
-
 <!DOCTYPE html>
  <!--Githubトークン----ccd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 -->
 <html lang="ja">
@@ -48,7 +16,7 @@ $view_tweets = [
         <!--検索エリア-->
         <form action="search.php" method="get">
             <div class="search-area">
-                <input type="text" class="form-control" placeholder="キーワード検索" name="keyword" value="">
+                <input type="text" class="form-control" placeholder="キーワード検索" name="keyword" value="<?php echo htmlspecialchars($view_keyword); ?>">
                 <button type="submit" class="btn">検索</button>
             </div>
         </form>
