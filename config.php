6ee11f67f2e8117c40dea25fcde6fe903e8800dd
SONOE385 <kotarou385@gmail.com>
2022-07-07 16:59:27 +0900

    herokuデプロイ

--- config.php
+++ config.php
@@ -6,10 +6,10 @@ date_default_timezone_set('Asia/Tokyo');
 //URLを定数に置き換える
 define('HOME_URL','/TwitterClone/');
 //データベースの接続情報
-define('DB_HOST','ec2-34-239-241-121.compute-1.amazonaws.com');
-define('DB_USER','ngxphyikbayzyu');
-define('DB_PASSWORD','a2e7419a1005cde7420ab8559e6824fa1c2085f0ca388ec0ac8ccbc94a29fea5');
-define('DB_NAME','twitter_clone');
+define('DB_HOST',getenv('DB_HOST'));
+define('DB_USER',getenv('DB_USER'));
+define('DB_PASSWORD',getenv('DB_PASSWORD'));
+define('DB_NAME',getenv('DB_DATABASE'));
 
 //9 行目から下、getenv('DB_HOST')に変える
 
